df0be4ae0bef64480c3a0fc5fd0b116e972f5c23
fjolivo <fjolivo@gmail.com>
2022-12-22 22:55:16 -0400

    Testing
    Trying to solve a problem after Shazam deprecation

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+// import React from 'react';
+// import { useParams } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+// import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
-import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
+// import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
 
-const ArtistDetails = () => {
-  const { id: artistId } = useParams();
-  const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const { data: artistData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery(artistId);
+// const ArtistDetails = () => {
+//   const { id: artistId } = useParams();
+//   const { activeSong, isPlaying } = useSelector((state) => state.player);
+//   const { data: artistData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery(artistId);
 
-  if (isFetchingArtistDetails) return <Loader title="Loading artists details" />;
+//   if (isFetchingArtistDetails) return <Loader title="Loading artists details" />;
 
-  if (error) return <Error />;
+//   if (error) return <Error />;
 
-  return (
-    <div className="flex flex-col">
-      <DetailsHeader
-        artistId={artistId}
-        artistData={artistData}
-      />
+//   return (
+//     <div className="flex flex-col">
+//       <DetailsHeader
+//         artistId={artistId}
+//         artistData={artistData}
+//       />
 
-      <RelatedSongs
-        data={Object.values(artistData?.songs)}
-        artistId={artistId}
-        isPlaying={isPlaying}
-        activeSong={activeSong}
-      />
+//       <RelatedSongs
+//         data={Object.values(artistData?.songs)}
+//         artistId={artistId}
+//         isPlaying={isPlaying}
+//         activeSong={activeSong}
+//       />
 
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
-export default ArtistDetails;
+// export default ArtistDetails;
